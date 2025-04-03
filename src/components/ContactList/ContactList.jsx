d315ccb1d1c57293f468ec02bba306e81ad474b6
Inna-Martunyuk <imartunyuk88@gmail.com>
2025-04-03 20:49:35 +0300

    hw-7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectVisibleContacts,
-  selectContacts,
-} from "../../redux/contactsSlice";
-import { fetchContacts } from "../../redux/contactsOps";
+
+import {  useSelector } from "react-redux";
+import {selectVisibleContacts } from "../../redux/contactsSlice";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
 const ContactList = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
   const filteredContacts = useSelector(selectVisibleContacts);
-
-  useEffect(() => {
-    if (contacts.length === 0) {
-      dispatch(fetchContacts());
-    }
-  }, [contacts.length, dispatch]); 
 
   if (!filteredContacts.length) {
     return <p className={css.message}>No contacts found.</p>;
